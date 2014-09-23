693b3a954869b5fb690278ce39a1cb300740edbe
mitsuru <mitsuru793@gmail.com>
2014-09-23 18:55:56 +0900

    ColumnOfBalls_002を追加

---
+++ Java/ColumnOfBalls_002/ColumnOfBalls.java
@@ -0,0 +1,25 @@
+import java.awt.*;
+import javax.swing.*;
+
+public class ColumnOfBalls extends JFrame {
+    public ColumnOfBalls() {
+        // タイトルを設定
+        setTitle("アニメーションの開始を遅らせる");
+        // サイズ変更禁止
+        setResizable(false);
+
+        // パネルを作成してフレームに追加
+        MainPanel panel = new MainPanel();
+        Container contentPane = getContentPane();
+        contentPane.add(panel);
+
+        // パネルサイズに合わせてフレームサイズを自動設定
+        pack();
+    }
+
+    public static void main(String[] args) {
+        ColumnOfBalls frame = new ColumnOfBalls();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
+}
---
+++ Java/ColumnOfBalls_002/MainPanel.java
@@ -0,0 +1,85 @@
+import java.awt.Dimension;
+import java.awt.Graphics;
+
+import javax.swing.JPanel;
+
+public class MainPanel extends JPanel implements Runnable {
+	// パネルサイズ
+	public static final int WIDTH = 240;
+	public static final int HEIGHT = 240;
+	// ボールの数
+	private static int NUM_BALL;
+	// ボールを格納する配列
+	private Ball[] ball;
+	// アニメーション用スレッド
+	private Thread thread;
+
+	// 動き出したボールの数
+	private int numMovingBall;
+	// 何ミリ秒後にアニメーションを開始するか
+	private int startTime = 1500;
+
+	public MainPanel() {
+		// パネルの推奨サイズを設定、pack()するときに必要
+		setPreferredSize(new Dimension(WIDTH, HEIGHT));
+		setSize(WIDTH, HEIGHT);
+
+		// 縦一列に作成できるボールの数を求める
+		NUM_BALL = HEIGHT / Ball.getSize();
+		System.out.println(NUM_BALL);
+		// ボールを格納する配列を作成
+		ball = new Ball[NUM_BALL];
+		// ボールを作成
+		for (int i = 0; i < NUM_BALL; i++) {
+			ball[i] = new Ball(0, Ball.getSize() * i, 1, 0);
+		}
+
+		numMovingBall = 0;
+
+		// スレッドを起動
+		thread = new Thread(this);
+
+		thread.start();
+	}
+
+	public void paintComponent(Graphics g) {
+		super.paintComponent(g);
+
+		// 各ボールを描画
+		for (int i = 0; i < NUM_BALL; i++) {
+			ball[i].draw(g);
+		}
+	}
+
+	// メインループ
+	public void run() {
+		// アニメーションの開始時間を遅らせる
+		try {
+			Thread.sleep(startTime);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+
+		// プログラムが終了するまでフレーム処理を繰り返す
+		while (true) {
+			// 各ボールを速度分だけ移動させる
+			for (int i = 0; i < numMovingBall; i++) {
+				ball[i].move();
+			}
+			// 動き始めたボールをカウント
+			if (numMovingBall < NUM_BALL) {
+				numMovingBall++;
+			}
+
+			// ボールを再描画
+			repaint();
+
+			// 10ミリ秒だけ休止
+			try {
+				Thread.sleep(10);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+}
